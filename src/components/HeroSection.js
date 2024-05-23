f6ce3762e2163c75d1d7b0157a75a462db39d058
Hussain Haider <hhaider740@gmail.com>
2024-05-24 05:13:43 +0530

    first1

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <div className="hero-section">
-      <img src="haiderprofile.jpg" alt="Hero Image" />
+      <img src="haiderprofile.jpg" />
       <h1>Hasan Haider</h1>
       <h3>[Senior SOftware Developer]</h3>
       <button>Contact Me</button>
